1f0887916d6de954f0dc5368043fedeacdd48e91
Luis Vinicius <luisvpersy@gmail.com>
2024-08-26 09:50:04 -0300

    first commit

---
+++ src/repository.py
@@ -0,0 +1,69 @@
+from bson import ObjectId
+from pymongo import MongoClient
+
+class Repository:
+  def __init__(self):
+    # Conectar ao banco de dados
+    client = MongoClient("mongodb+srv://vini:kIBaEiozt0Jq60Jp@cluster0.2zppx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    db = client['Trials']
+
+    self.ClientCollection  = db['Clients']
+    self.productCollection = db['Products']
+
+  def _create_client(self, domain, tenantCreation, GDAP, CobrançaRecorrente, Info, Obs, emailAdmin):
+    if self.ClientCollection.find_one({'domain': domain}):
+      raise Exception('Client already exists')
+
+    self.ClientCollection.insert_one({
+      'domain'            : domain,
+      'tenantCreation'    : tenantCreation,
+      'produtos'          : [],
+      'GDAP'              : GDAP,
+      'CobrançaRecorrente': CobrançaRecorrente,
+      'Info'              : Info,
+      'Obs'               : Obs,
+      'emailAdmin'        : emailAdmin
+    })
+
+  def _find_clients(self):
+    return list(self.ClientCollection.find())
+
+  def _find_client_by_domain(self, domain):
+    return self.ClientCollection.find_one({'domain': domain})
+
+  def _find_client_by_id(self, _id):
+    _id = ObjectId(_id)
+    return self.ClientCollection.find_one({'_id': _id})
+  
+  def _update_client(self, filter, update):
+    self.ClientCollection.update_one(filter, update)
+    
+  def _delete_client(self, _id):
+    self.ClientCollection.delete_one({'_id': _id})
+
+  def _delete_client_by_domain(self, domain):
+    self.ClientCollection.delete_one({'domain': domain})
+  
+  def _create_product(self, name, price):
+    if self.productCollection.find_one({'name': name}):
+      raise Exception('Product already exists')
+
+    self.productCollection.insert_one({
+    'name'  : name,
+    'price' : price
+    })
+
+  def _find_products(self):
+    return list(self.productCollection.find())
+
+  def _find_product_by_name(self, name):
+    return self.productCollection.find_one({'name': name})
+  
+  def _find_product_by_id(self, _id):
+    return self.productCollection.find_one({'_id': _id})
+
+  def _update_product(self, filter, update):
+    self.productCollection.update_one(filter, update)
+
+  def _delete_product(self, _id):
+    self.productCollection.delete_one({'_id': _id})
---
+++ src/service.py
@@ -0,0 +1,71 @@
+import datetime
+from bson import ObjectId
+import pytz
+from repository import Repository
+
+class Service(Repository):
+  def __init__(self):
+    super().__init__()
+
+  def register_client(self, domain, tenantCreation, GDAP, CobrançaRecorrente, Info, Obs, emailAdmin):
+    if self._find_client_by_domain(domain):
+      raise Exception('Client already exists')
+    self._create_client(domain, tenantCreation, GDAP, CobrançaRecorrente, Info, Obs, emailAdmin)
+
+  def list_clients(self):
+    return self._find_clients()
+
+  def find_client_with_domain(self, domain):
+    return self._find_client_by_domain(domain)
+  
+  def find_client_with_id(self, id):
+    return self._find_client_by_id(id)
+
+  def update_client_by_id(self, id, fields):
+    id = ObjectId(id)
+    self._update_client({'_id': id}, {'$set': fields})
+
+  def add_product_to_client(self, domain, id_product, date_renovation, licences, price = None):
+    product = self._find_product_by_id(id_product)
+    
+    if not product:
+      raise Exception('Product not found')
+    
+    date =self._convert_date_time(date_renovation)
+    product['date_renovation'] = date
+
+    product['licences'] = licences
+    product['price'] = price if price else product['price']
+
+    client = self._find_client_by_domain(domain)
+    client['produtos'].append(product)
+    self._update_client({'domain': domain}, {'$set': {'produtos': client['produtos']}})
+
+  def remove_product_from_client(self, domain, id_product):
+    client = self._find_client_by_domain(domain)
+    client['produtos'] = list(filter(lambda product: product['_id'] != id_product, client['produtos']))
+    self._update_client({'domain': domain}, {'$set': {'produtos': client['produtos']}})
+
+  def register_product(self, name, price):
+    if self._find_product_by_name(name):
+      raise Exception('Product already exists')
+
+    self._create_product(name, price)
+
+  def list_products(self):
+    return self._find_products()
+  
+  def _convert_date_time(self, date):
+    date_obj = datetime.datetime.strptime(date, '%d/%m/%Y')
+    date_utc = date_obj.replace(tzinfo=pytz.UTC)
+    return date_utc
+
+  def get_clients_by_date_products(self) -> list[object]:
+    clients = self._find_clients()
+
+    def get_min_date(client):
+      product_dates = [product['date_renovation'] for product in client['produtos']]
+      return min(product_dates)
+
+    clients.sort(key=lambda client: get_min_date(client))
+    return clients
